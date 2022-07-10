a2a0cb39c00fa07244604239fcd5703c2d6641af
Samson Akinsanya <info@mobeparadigm.co.uk>
2022-07-10 12:24:04 +0100

    Initial Commit

---
+++ app/src/main/java/com/haehome/model/InstalledApp.kt
@@ -0,0 +1,4 @@
+package com.haehome.model
+
+class InstalledApp {
+}
---
+++ app/src/main/java/com/haehome/ui/AppIcon.kt
@@ -0,0 +1,6 @@
+package com.haehome.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppIcon(installedApp: )
